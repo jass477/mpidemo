7d1bb0085f64c82f959c63ffa14926e2df057255
jassdev <sdking477@qq.com>
2020-08-12 12:42:27 +0800

    Update hello.c

--- code/hello.c
+++ code/hello.c
@@ -5,5 +5,11 @@ int main(int argc, char* argv[])
 
     print("hello");
     print("world");
+        print("hello");
+    print("world");
+        print("hello");
+    print("world");
+        print("hello");
+    print("world");
 
 } 
